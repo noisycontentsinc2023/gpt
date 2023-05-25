d3ee2d6d0a296b4ede9db29dc9fbfb6ac5c42100
piaotaijiong <127836167+piaotaijiong@users.noreply.github.com>
2023-05-25 16:07:46 +0900

    Update gpt.py

--- gpt.py
+++ gpt.py
@@ -1,7 +1,6 @@
 import openai
 import discord
 import os
-import asyncio
 from discord.ext import commands
 
 # Load your OpenAI and Discord Tokens
@@ -15,37 +14,41 @@ intents.message_content = True
 bot = commands.Bot(command_prefix=PREFIX, intents=intents)
 
 openai.api_key = OPENAI
-CHANNEL_IDS = [1111123852546805800, 1111138777453305967]   # Replace with your channel id
+CHANNEL_ID = 1111138777453305967  # Replace with your channel id
 
-user_sessions = {}
-
+user_messages = {}
 
 @bot.event
 async def on_message(message):
     if message.author == bot.user:
         return
 
-    if message.channel.id in CHANNEL_IDS:
+    if message.channel.id == CHANNEL_ID:
         # Send the 'I'm thinking' message
         thinking_message = await message.channel.send(f"{message.author.mention}님의 질문에 대해 생각하는 중...")
 
-        # Initialize a session if there's none
-        if message.author.id not in user_sessions:
-            user_sessions[message.author.id] = [{"role": "system", "content": "You are a helpful assistant."}]
-            
-        user_sessions[message.author.id].append({
+        # Append user message to their messages list
+        if message.author.id not in user_messages:
+            user_messages[message.author.id] = []
+        user_messages[message.author.id].append({
             "role": "user",
             "content": message.content
         })
 
+        # Generate a conversation history for GPT-3.5
+        conversation_history = [
+            {"role": "system", "content": "You are a helpful assistant."},
+        ] + user_messages[message.author.id]
+
         # Generate a message from GPT-3.5
-        response = openai.ChatCompletion.create(
+        loop = asyncio.get_event_loop()
+        response = await loop.run_in_executor(None, openai.ChatCompletion.create, 
             model="gpt-3.5-turbo",
-            messages=user_sessions[message.author.id],
+            messages=conversation_history,
         )
 
-        # Append AI message to the user's session
-        user_sessions[message.author.id].append({
+        # Append AI message to the user's messages list
+        user_messages[message.author.id].append({
             "role": "assistant",
             "content": response.choices[0].message['content']
         })
@@ -54,9 +57,15 @@ async def on_message(message):
         await thinking_message.delete()
 
         # Create an embed message
+        response_text = response.choices[0].message['content']
+
+        # Truncate the response_text if it is too long
+        if len(response_text) > 2000:
+            response_text = response_text[:1997] + '...'
+
         embed = discord.Embed(
             title="ChatGPT Response",
-            description=f"{message.author.mention}님의 질문에 대한 답변입니다\n{response.choices[0].message['content']}",
+            description=f"{message.author.mention}님의 질문에 대한 답변입니다\n{response_text}",
             color=discord.Color.blue()
         )
         embed.set_footer(text="이 답변은 ChatGPT 3.5 모델로 작성되었습니다")
@@ -64,3 +73,5 @@ async def on_message(message):
 
     # Process commands after the message event
     await bot.process_commands(message)
+
+bot.run(TOKEN)

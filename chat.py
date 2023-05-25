b4492e3295db94c242aeee52e15c74906f86d408
piaotaijiong <127836167+piaotaijiong@users.noreply.github.com>
2023-05-25 18:04:28 +0900

    Update chat.py

--- chat.py
+++ chat.py
@@ -19,3 +19,43 @@ bot = commands.Bot(command_prefix=PREFIX, intents=intents)
 
 openai.api_key = OPENAI
 CHANNEL_IDS = [1111123852546805800, 1111138777453305967]   # Replace with your channel id
+
+ongoing_conversations = {}
+
+@bot.event
+async def on_message(message):
+    # Ignore messages from the bot itself
+    if message.author == bot.user:
+        return
+
+    # Only respond in the specified channel
+    if message.channel.id != CHANNEL_IDS:
+        return
+
+    # Initialize the conversation if needed
+    if message.author.name not in ongoing_conversations:
+        ongoing_conversations[message.author.name] = [{"role": "system", "content": "You are a helpful assistant."}]
+
+    # Add the user's message to the conversation
+    ongoing_conversations[message.author.name].append({
+        "role": "user",
+        "content": message.content
+    })
+
+    # Generate a response
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=ongoing_conversations[message.author.name],
+    )
+
+    # Add the bot's response to the conversation
+    ongoing_conversations[message.author.name].append({
+        "role": "assistant",
+        "content": response.choices[0].message['content']
+    })
+
+    # Send the response
+    await message.channel.send(response.choices[0].message['content'])
+
+# Run the bot
+bot.run(TOKEN)

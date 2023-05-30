7a7376f9cfe853a22e74d0cd2eb65878190f120d
piaotaijiong <127836167+piaotaijiong@users.noreply.github.com>
2023-05-30 11:58:11 +0900

    Update chat.py

--- chat.py
+++ chat.py
@@ -30,7 +30,7 @@ ongoing_conversations = {}
 async def on_message(message):
     global reset_time
     # Ignore messages from the bot itself
-    if message.author == bot.user:
+    if message.author.id == bot.user.id:
         return
 
     # Only respond in the specified channel
